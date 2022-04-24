6b618fc171c0bb201934041d44d31a87506d9d81
Egoist <epsilonelittr@gmail.com>
2022-04-24 04:25:20 +0300

    Add theme page and related styles

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,8 +2,6 @@ require("laravel-mix-purgecss");
 
 const mix = require("laravel-mix");
 const tailwindcss = require("tailwindcss");
-const fs = require("fs");
-const path = require("path");
 
 const publicWebAssetsFolder = "public";
 
@@ -11,6 +9,7 @@ mix
   .sass(`./sass/error.scss`, `${publicWebAssetsFolder}/css/error.min.css`)
   .sass(`./sass/landing.scss`, `${publicWebAssetsFolder}/css/landing.min.css`)
   .sass(`./sass/category.scss`, `${publicWebAssetsFolder}/css/category.min.css`)
+  .sass(`./sass/theme.scss`, `${publicWebAssetsFolder}/css/theme.min.css`)
   .options({
     processCssUrls: false,
     postCss: [tailwindcss("./tailwind.config.js")],
